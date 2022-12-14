b791dc28a49c46ac8e6e9e4909b227abe3e1d2d1
Martin Abbrent <martin.abbrent@ufz.de>
2022-12-14 22:04:03 +0100

    Initial commit of the tsmdl fork from https://codebase.helmholtz.cloud/hub-terra/tsmdl-time-series-management-decoupling-layer/tsmdl-api

---
+++ app/main.py
@@ -0,0 +1,287 @@
+"""
+Prototype for an tsm mapping api.
+
+The base purpose of this is to have a common interface
+to query information about tsm datastreams in the SMS.
+
+For each centre they are organized in datasources (db + server),
+things & datastreams themselves.
+
+It is also the question if we need the observations to be
+accessible with this api - this depends on the sync mechanism
+to the frost server.
+
+The APIs here doesn't include pagination for now.
+"""
+import logging
+import uuid
+from datetime import datetime
+from typing import Optional, List
+from fastapi import FastAPI, HTTPException
+from pydantic import BaseModel, Field
+import psycopg2
+import psycopg2.extras
+import psycopg2.sql
+import uvicorn
+
+app = FastAPI(version="0.2.0")
+
+
+class Message(BaseModel):
+    detail: str
+
+
+class Datasource(BaseModel):
+    id: str = Field(alias="@iot.id")
+    name: str
+    description: Optional[str]
+    properties: dict
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class DatasourceList(BaseModel):
+    value: List[Datasource]
+
+
+class Thing(BaseModel):
+    id: str = Field(alias="@iot.id")
+    name: str
+    description: Optional[str]
+    properties: dict
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class ThingList(BaseModel):
+    value: List[Thing]
+
+
+class Datastream(BaseModel):
+    id: str = Field(alias="@iot.id")
+    name: str
+    description: Optional[str]
+    properties: dict
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class DatastreamList(BaseModel):
+    value: List[Datastream]
+
+
+class Observation(BaseModel):
+    id: str = Field(alias="@iot.id")
+    result_time: datetime = Field(alias="resultTime")
+    result: float
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class ObservationList(BaseModel):
+    value: List[Observation]
+
+
+#         self.db = psycopg2.connect(database_settings.get('url'))
+conn = psycopg2.connect("postgres://postgres:postgres@host.docker.internal/postgres")
+# conn = psycopg2.connect("postgres://postgres:postgres@localhost/postgres")
+# FakeDB
+db = {
+    "influxdb_01": {
+        "altPlestlin": {
+            "AirTemp": [
+                (datetime(year=2022, month=1, day=1), 1.0),
+                (datetime(year=2022, month=1, day=2), 2.0),
+            ],
+            "AirHumidity": [],
+            "LeafWetness": [],
+            "SoilMosture_0.1m": [],
+        },
+        "altTellin": {},
+        "beestland": {},
+        "zeitlow-BF1": {},
+    },
+    "influxdb_02": {},
+    "influxdb_03": {},
+    "timescaleDB__db01": {},
+    "timescaleDB__db02": {},
+}
+
+
+@app.get("/Datasources", response_model=DatasourceList)
+def get_datasources():
+    """
+    Return the datasources in that tsm instance.
+
+    A datasource can be a database for example.
+    """
+    result = []
+
+    with conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("""
+        select schemaname          as id,
+               ''                  as name,
+               ''                  as description,
+               json_build_object() as properties
+        from pg_tables t
+        where t.tablename = 'thing';
+        """)
+        for row in cur.fetchall():
+            result.append(Datasource(
+                id=row.get('id'),
+                name=row.get('name'),
+                description=row.get('description'),
+                properties=row.get('properties')
+            ))
+
+    return DatasourceList(value=result)
+
+
+@app.get(
+    "/Datasources({datasource_id})/Things",
+    response_model=ThingList,
+    responses={"404": {"model": Message}},
+)
+def get_things(datasource_id: str):
+    """
+    Return the things of a tsm datasource.
+
+    Things can be measurement configurations, stations, etc.
+    """
+    # Validate for a valid datasource id
+    check_datasource_id(datasource_id)
+
+    result = []
+    
+    with conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute(psycopg2.sql.SQL("""
+        select uuid        as id,
+               name        as name,
+               description as description,
+               properties  as properties
+        from {schema}.thing;
+        """).format(schema=psycopg2.sql.Identifier(datasource_id)))
+
+        for row in cur.fetchall():
+            result.append(Thing(
+                id=row.get('id'),
+                name=row.get('name'),
+                description=row.get('description'),
+                properties=row.get('properties')
+            ))
+
+    return ThingList(value=result)
+
+
+def check_datasource_id(datasource_id):
+    with conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute("""
+        select t.schemaname
+        from pg_tables t
+        where t.tablename = 'thing' and t.schemaname = %(schema)s;
+        """, {'schema': datasource_id})
+        if not cur.fetchone():
+            raise HTTPException(status_code=404, detail="Datasource not found")
+
+
+@app.get(
+    "/Datasources({datasource_id})/Things({thing_id})/Datastreams",
+    response_model=DatastreamList,
+    responses={"404": {"model": Message}},
+)
+def get_datastreams(datasource_id: str, thing_id: str):
+    """
+    Get the datastreams of a thing.
+
+    A datastream is the series of measurments.
+    An example is airtemperature (datastream) of a station (thing).
+    """
+    # Validate datasource id
+    check_datasource_id(datasource_id)
+
+    # Validate thing id
+    check_thing_id(datasource_id, thing_id)
+
+    result = []
+
+    with conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        cur.execute(psycopg2.sql.SQL("""
+        select d.id,
+               d.name,
+               d.description,
+               json_build_object(
+                       'column_headers', json_agg(distinct o.parameters ->> 'column_header'),
+                       'position', d.position,
+                       'created_at', d.properties ->> 'created_at'
+                   ) as properties
+        from {schema}.thing t
+                 join {schema}.datastream d on t.id = d.thing_id
+                 left join {schema}.observation o on d.id = o.datastream_id
+        where t.uuid = %(thing_id)s
+        group by d.id, d.name, d.description
+        order by d.id;
+        """).format(schema=psycopg2.sql.Identifier(datasource_id)), {'thing_id': thing_id})
+
+        for row in cur.fetchall():
+            result.append(Datastream(
+                id=row.get('id'),
+                name=row.get('name'),
+                description=row.get('description'),
+                properties=row.get('properties')
+            ))
+
+    return DatastreamList(value=result)
+
+
+def check_thing_id(datasource_id, thing_id):
+    try:
+        uuid.UUID(str(thing_id))
+    except ValueError:
+        raise HTTPException(status_code=406, detail="Thing uuid format not properly")
+
+    with conn:
+        cur = conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+        try:
+            cur.execute(psycopg2.sql.SQL("""
+            select uuid from {schema}.thing where uuid = %(thing_id)s;
+            """).format(schema=psycopg2.sql.Identifier(datasource_id)),
+                        {'thing_id': thing_id})
+        except psycopg2.errors.InvalidTextRepresentation:
+            raise HTTPException(status_code=406, detail="Thing uuid format not properly")
+
+        if not cur.fetchone():
+            raise HTTPException(status_code=404, detail="Thing not found")
+
+
+@app.get(
+    "/Datasources({datasource_id})/Things({thing_id})/Datastreams({datastream_id})/Observations",
+    response_model=ObservationList,
+    responses={"404": {"model": Message}, "501": {"model": Message}},
+)
+def get_observations(
+    datasource_id: str,
+    thing_id: str,
+    datastream_id: str,
+):
+    """
+    Return the observations of a datastream.
+
+    An observation contains a measured value associated with a timestamp.
+    """
+    check_datasource_id(datasource_id)
+    check_thing_id(thing_id)
+
+    raise HTTPException(status_code=501, detail="Not yet implemented")
+
+    return ObservationList(value=result_list)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
